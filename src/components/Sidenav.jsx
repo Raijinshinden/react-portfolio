8c0ce80476c967fbaa3ec4411ef9a11e5cc7709b
Ryan Dempsey <rjd9896@gmail.com>
2024-04-19 00:04:36 +0900

    Update Sidenav.jsx

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
-import {AiOutlineMenu, AiOutlineHome} from 'react-icons/ai'
+import {AiOutlineMenu, AiOutlineHome, AiOutlineProject, AiOutlineMail} from 'react-icons/ai'
+import {BsPerson} from 'react-icons/bs'
 import {GrProjects} from 'react-icons/gr'
 
 function Sidenav() {
@@ -26,7 +27,7 @@ function Sidenav() {
                       href="main" 
                       className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'
                     >
-                      <AiOutlineHome size={20} />
+                      <GrProjects size={20} />
                       <span className='pl-4'>Work</span>
                     </a>
                     <a 
