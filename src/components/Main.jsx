da9de7565bb675f7184dbeba5d8edc9cf199d2d2
Ryan Dempsey <rjd9896@gmail.com>
2024-04-19 00:54:45 +0900

    Changed the background img for main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Main() {
   return (
     <div id='main'>
-        <img src='https://images.unsplash.com/photo-1560631820-ecf9d56ba39e?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' />
+        <img className='' src='https://images.unsplash.com/photo-1464746133101-a2c3f88e0dd9?q=80&w=843&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' />
     </div>
   )
 }
