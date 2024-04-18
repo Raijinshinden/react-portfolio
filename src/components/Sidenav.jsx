fae975129d69d03874fa1395f5a34c5f63457664
Ryan Dempsey <rjd9896@gmail.com>
2024-04-19 00:07:32 +0900

    Update Sidenav.jsx

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -34,7 +34,7 @@ function Sidenav() {
                       href="main" 
                       className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'
                     >
-                      <AiOutlineHome size={20} />
+                      <AiOutlineProject size={20} />
                       <span className='pl-4'>Projects</span>
                     </a>
                     <a 
