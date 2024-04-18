3ebf03746eff70e87bba36947fb7db569d409453
Ryan Dempsey <rjd9896@gmail.com>
2024-04-18 23:41:12 +0900

    Update Sidenav.jsx

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -10,8 +10,15 @@ function Sidenav() {
 
   return (
     <div>
-        <AiOutlineMenu />
-
+        <AiOutlineMenu onClick={handleNav} className='absolute top-4 right-4 z-[99] md:hidden'/>
+        {
+            nav ? (
+                <div></div>
+            )
+            : (
+                <div></div>
+            )
+        }
     </div>
   );
 };
