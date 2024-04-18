9f2bde3de38d0a22482305bda8dac437b93dd190
Ryan Dempsey <rjd9896@gmail.com>
2024-04-19 00:49:30 +0900

    Update Main.jsx

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function Main() {
   return (
-    <div>Main</div>
+    <div id='main'>
+        <img src='https://images.unsplash.com/photo-1560631820-ecf9d56ba39e?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' />
+    </div>
   )
 }
 
