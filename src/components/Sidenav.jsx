fd95533c215cef5358e7f2771b41cf460d7b52ad
Ryan Dempsey <rjd9896@gmail.com>
2024-04-18 23:31:44 +0900

    update

---
+++ src/components/Sidenav.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {AiOutlineMenu} from 'react-icons/ai'
+
+function Sidenav() {
+  return (
+    <div>
+        <AiOutlineMenu />
+    </div>
+  )
+}
+
+export default Sidenav
