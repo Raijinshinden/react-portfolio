9c46d8de134a57aa5eb85c62e7cb90b03b7c5a1c
Ryan Dempsey <rjd9896@gmail.com>
2024-04-19 00:32:26 +0900

    Update Sidenav.jsx

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -59,7 +59,19 @@ function Sidenav() {
             <div className='md:block hidden fixed top-[25%] z-10'>
                 <div className='flex flex-col'>
                     <a href="#main" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
-                        <AiOutlineHome />
+                        <AiOutlineHome size={20}/>
+                    </a>
+                    <a href="#main" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
+                        <AiOutlineHome size={20}/>
+                    </a>
+                    <a href="#main" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
+                        <AiOutlineHome size={20}/>
+                    </a>
+                    <a href="#main" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
+                        <AiOutlineHome size={20}/>
+                    </a>
+                    <a href="#main" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
+                        <AiOutlineHome size={20}/>
                     </a>
                 </div>
             </div>
