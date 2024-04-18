f84b56ba88258916f428bd1d9441c69588a1397f
Ryan Dempsey <rjd9896@gmail.com>
2024-04-18 23:36:37 +0900

    Update Sidenav.jsx

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {AiOutlineMenu} from 'react-icons/ai'
 
 function Sidenav() {
+
+    const [nav, setNav] = useState(false)
+    const handleNav = () => {
+        setNav(!nav)
+    }
+
   return (
     <div>
         <AiOutlineMenu />
+
     </div>
   )
 }
