a0ba587d82f8e95be7afd15f52a1f3c0cffa6d2c
Ryan Dempsey <rjd9896@gmail.com>
2024-04-21 00:07:58 +0900

    Update Main.jsx

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -10,8 +10,8 @@ function Main() {
         />
         <div className='w-full h-screen absolute top-0 left-0 bg-white/50'>
           <div className='max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-start items-center'>
-            <h1>I'm Ryan</h1>
-            <h2>
+            <h1 className='sm:text-5xl text-4xl font-bold text-gray-800'>I'm Ryan</h1>
+            <h2 className='flex sm:text-3xl text-2xl pt-4 text-gray-800'>
               I'm a 
               <TypeAnimation
                 sequence={[
@@ -29,7 +29,7 @@ function Main() {
                 repeat={Infinity}
                 />
             </h2>
-            <div>
+            <div className='flex justify-between pt-6 max-w-[200px] w-full'>
                 <FaTwitter className='cursor-pointer' size={20}/>
                 <FaFacebook className='cursor-pointer' size={20}/>
                 <FaInstagram className='cursor-pointer' size={20}/>
