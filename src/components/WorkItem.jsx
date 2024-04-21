aad99d36499fe55274746bcaccfcbeb70f80b585
Ryan Dempsey <rjd9896@gmail.com>
2024-04-22 00:10:37 +0900

    Update WorkItem.jsx

--- src/components/WorkItem.jsx
+++ src/components/WorkItem.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const WorkItem = () => {
   return (
-    <ol className='flex flex-col md:flex-row relative border-l border-stone-200'>WorkItem</ol>
+    <ol className='flex flex-col md:flex-row relative border-l border-stone-200'>
+        <li>
+            
+        </li>
+    </ol>
   )
 }
 
