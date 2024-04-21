6c424050e46d72bfe70788b631a8616fecf2944a
Ryan Dempsey <rjd9896@gmail.com>
2024-04-22 00:14:35 +0900

    Update WorkItem.jsx

--- src/components/WorkItem.jsx
+++ src/components/WorkItem.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 
-const WorkItem = () => {
+const WorkItem = ({year, title, duration, details}) => {
   return (
     <ol className='flex flex-col md:flex-row relative border-l border-stone-200'>
         <li>
             <div />
             <p>
                 <span>{year}</span>
+                <span>{title}</span>
+                <span>{duration}</span>
             </p>
+            <p>{details}</p>
         </li>
     </ol>
   )
