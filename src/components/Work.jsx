5672e43d78ecbf5f9f629c79429b118a6524dc85
Ryan Dempsey <rjd9896@gmail.com>
2024-04-21 23:49:13 +0900

    Update Work.jsx

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,6 +1,15 @@
 import React from 'react'
 import WorkItem from './WorkItem'
 
+
+const data = [
+    {
+        year: 2022,
+        title: "Head English Teacher",
+        duration: "2 years",
+        details: "I taught English to a variety of students from kindergarten age to adults."
+    }
+]
 const Work = () => {
   return (
     <div>Work</div>
