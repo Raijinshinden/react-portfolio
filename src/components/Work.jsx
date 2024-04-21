cf0b80ea2d320784daf035e50ef6dfedeb7ea72e
Ryan Dempsey <rjd9896@gmail.com>
2024-04-21 23:58:17 +0900

    Update Work.jsx

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -20,7 +20,7 @@ const Work = () => {
   return (
     <div id="work" className='max-w-[1040px] mg-auto md:pl-20 p-4 py-16'>
         <h1 className='text-4xl font-bold text-[#001b5e]'>Work</h1>
-        {data.map}
+        {data.map(() =>)}
     </div>
   )
 }
