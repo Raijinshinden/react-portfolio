70f066a2734c33144424334a29cdddc8cf35cd2d
Ryan Dempsey <rjd9896@gmail.com>
2024-04-21 23:57:01 +0900

    Update Work.jsx

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -19,7 +19,7 @@ const data = [
 const Work = () => {
   return (
     <div id="work" className='max-w-[1040px] mg-auto md:pl-20 p-4 py-16'>
-        <h1 className='text-4xl font-bold text-[#]'>Work</h1>
+        <h1 className='text-4xl font-bold text-[#001b5e]'>Work</h1>
     </div>
   )
 }
