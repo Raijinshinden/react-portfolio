ac784023495a84700d655e1b4b6625cf10e7bb43
Ryan Dempsey <rjd9896@gmail.com>
2024-04-22 23:57:22 +0900

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,7 +7,12 @@ const Projects = () => {
   return (
     <div id='projects' className='max-w-[1040px] m-auto md:pl-20 p-4 py-16'>
         <h1 className='text-4xl font-bold text-center text-[#001b5e]'>Projects</h1>
-        <p className='text-center py-8'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem, rem quibusdam amet deleniti deserunt cum cumque error porro id voluptates eveniet sequi fugiat voluptatibus. Vero sunt unde asperiores nam quidem?</p>
+        <p className='text-center py-8'>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem, rem quibusdam amet deleniti deserunt cum cumque error porro id voluptates eveniet sequi fugiat voluptatibus. Vero sunt unde asperiores nam quidem?
+        </p>
+        <div className=''>
+            <ProjectItem />
+        </div>
     </div>
   )
 }
