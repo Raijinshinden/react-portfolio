f99037da63d063e1d5cceed96da5c08c4e2bd271
Ryan Dempsey <rjd9896@gmail.com>
2024-04-22 23:55:39 +0900

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,8 +6,8 @@ import shopImg from '../assets/shop.jpg'
 const Projects = () => {
   return (
     <div id='projects' className='max-w-[1040px] m-auto md:pl-20 p-4 py-16'>
-        <h1>Projects</h1>
-        <p></p>
+        <h1 className='text-4xl font-bold text-center text-[#001b5e]'>Projects</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem, rem quibusdam amet deleniti deserunt cum cumque error porro id voluptates eveniet sequi fugiat voluptatibus. Vero sunt unde asperiores nam quidem?</p>
     </div>
   )
 }
