fa831d785fe3d064581bfbf3b57dc5b5b3eab54a
Ryan Dempsey <rjd9896@gmail.com>
2024-04-22 23:47:20 +0900

    Update Main.jsx

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -10,7 +10,7 @@ function Main() {
         />
         <div className='w-full h-screen absolute top-0 left-0 bg-white/50'>
           <div className='max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-start items-center'>
-            <h1 className='sm:text-5xl text-4xl font-bold text-gray-800'>I'm Ryan</h1>
+            <h1 className='sm:text-5xl text-4xl font-bold text-gray-800'>I'm Ryan Dempsey</h1>
             <h2 className='flex sm:text-3xl text-2xl pt-4 text-gray-800'>
               I'm a 
               <TypeAnimation
