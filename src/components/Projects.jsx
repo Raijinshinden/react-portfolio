0c93f3cc3f214baea6965ab40fd03257a2d221cc
Ryan Dempsey <rjd9896@gmail.com>
2024-04-22 23:52:07 +0900

    Added a shopping site picture

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ProjectItem from './ProjectItem'
 import gymImg from '../assets/gym.jpg'
+import shopImg from '../assets/shop.jpg'
 
 const Projects = () => {
   return (
